c61b6b3acaa43105c9d11fbb9e90098138573deb
CrisPlays80 <cristian.steven05@gmail.com>
2026-04-02 10:21:00 -0500

    Implement FastAPI app with root endpoint

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from url-shortener!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
