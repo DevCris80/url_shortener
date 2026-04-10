24db9f9fb411a5211b64804ca72d95256685455f
CrisPlays80 <cristian.steven05@gmail.com>
2026-04-10 10:12:21 -0500

    Add: SQLAlchemy 2.0 integration and new table "User", with foreign key

--- db/model_url.py
+++ db/model_url.py
@@ -1,14 +1,35 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from datetime import datetime
+from sqlalchemy import String, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db.base import Base
 
 class URL(Base):
     __tablename__ = "urls"
 
-    id = Column(Integer, primary_key=True, index=True)
-    long_url = Column(String, index=True)
-    short_url = Column(String, index=True)
-    clicks = Column(Integer, default=0)
-    created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    
+    long_url: Mapped[str] = mapped_column(String(2048), index=True)
+    short_url: Mapped[str] = mapped_column(String(20), index=True)
+    clicks: Mapped[int] = mapped_column(default=0)
+    
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), 
+        onupdate=func.now()
+    )
+    
+    user: Mapped["User"] = relationship(back_populates="urls")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    password_hash: Mapped[str] = mapped_column(String(255))
+    urls: Mapped[list["URL"]] = relationship(
+        back_populates="user", 
+        cascade= "all, delete-orphan"
+    )
