76bd7bcbea217f2d17109a809a6208a40c2aeabe
CrisPlays80 <cristian.steven05@gmail.com>
2026-04-12 20:29:49 -0500

    Router users

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from routes.shortener import router as url_router
+from routes.user import router as user_router
 
 app = FastAPI()
 
 app.include_router(url_router)
+app.include_router(user_router, prefix="/user", tags=["user"])
 
 @app.get("/")
 def read_root():
