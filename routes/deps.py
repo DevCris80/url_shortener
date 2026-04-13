4a35a87cad92df3e73f0a94188d7d8ab3d924e9e
CrisPlays80 <cristian.steven05@gmail.com>
2026-04-12 20:30:04 -0500

    Empezar el manejo de la autorizacion

--- routes/deps.py
+++ routes/deps.py
@@ -1,5 +1,12 @@
+from fastapi.security import OAuth2PasswordBearer
+
 from db.session import get_db
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="")
 
 async def get_database():
     async for db in get_db():
         yield db
+
+def get_current_user():
+    pass
