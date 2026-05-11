ecee4eef3ce93354148dba2c1106a8f18b1ef012
CrisPlays80 <cristian.steven05@gmail.com>
2026-05-11 14:11:00 -0500

    Rename user.py to user_router.py

--- routes/user_router.py
+++ routes/user_router.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter, Depends
-from typing import Annotated
 
-from routes.deps import get_current_user
-from schemas.user import UserRead
+from routes.deps import CurrentUser
 
 router = APIRouter()
 
 @router.get("/me")
-async def read_user_me(current_user = Annotated[UserRead, Depends(get_current_user)]):
+async def read_user_me(current_user: CurrentUser):
     return current_user
