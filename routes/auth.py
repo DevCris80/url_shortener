bb2994184ad39aef9b81d075b867e9b488e20cdf
CrisPlays80 <cristian.steven05@gmail.com>
2026-05-13 19:16:53 -0500

    refactor(auth): use UserService dependency for login

--- routes/auth.py
+++ routes/auth.py
@@ -4,20 +4,29 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from core.security import create_access_token
 from core.config import settings
-from service.user import authenticate_user
+from routes.deps import UserServiceDep, SessionDep
 from db.model_url import User
 from schemas.user import Token
 
 router = APIRouter(tags=["login"])
 
+
 @router.post("/login/access-token")
-async def login_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user: User = await authenticate_user(email=form_data.username, password=form_data.password)
+async def login_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    user_service: UserServiceDep,
+    db: SessionDep,
+):
+    user: User = await user_service.authenticate_user(
+        email=form_data.username, password=form_data.password, db=db
+    )
+
     if not user:
         raise HTTPException("Incorrect email or password")
     elif not user.is_active:
         raise HTTPException("Inactive User")
-    access_token_user = create_access_token(user.id, settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    return Token(
-        access_token= access_token_user
+
+    access_token_user = create_access_token(
+        user.id, settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
+    return Token(access_token=access_token_user)
