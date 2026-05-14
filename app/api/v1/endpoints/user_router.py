fee3a6af163abcb313ab3e73dd6acbfce9d5afcd
DevCris80 <cristian.steven05@gmail.com>
2026-05-14 12:20:24 -0500

    Endpoint Create user

--- app/api/v1/endpoints/user_router.py
+++ app/api/v1/endpoints/user_router.py
@@ -1,11 +1,24 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException
 
-from app.api.deps import CurrentUser
+from app.api.deps import SessionDep, CurrentUser, UserServiceDep
+from app.schemas.user import UserCreate
+from app.exceptions import user_exceptions
 
 router = APIRouter()
 
-@router.post("")
-async def create_user()
+@router.post("/")
+async def create_user(
+    user: UserCreate,
+    db: SessionDep,
+    user_service: UserServiceDep
+):
+    try:
+        return user_service.create_user(user, db)
+    except user_exceptions.UserAlreadyExistsError as e:
+        raise HTTPException(status_code=409, detail="User already exists")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Error in create user")
+
 @router.get("/me")
 async def read_user_me(current_user: CurrentUser):
     return current_user
