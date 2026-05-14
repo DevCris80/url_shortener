61cb0f8808a57c1bc3b8e893ec4caf8a9159e18c
CrisPlays80 <cristian.steven05@gmail.com>
2026-05-13 19:23:53 -0500

    move: relocate and update imports for app/db/session.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-from core.config import settings
+from app.core.config import settings
 
 engine = create_async_engine(settings.DB_URL, echo = True)
 AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession, expire_on_commit=False)
