929378d678627b35bf31390541e323bad22c0168
CrisPlays80 <cristian.steven05@gmail.com>
2026-05-13 19:16:42 -0500

    feat(shortener): check for existing long URL before creating new short URL

--- service/shorten.py
+++ service/shorten.py
@@ -10,11 +10,19 @@ class Shortener:
     CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     SHORT_URL_LENGTH = 6
 
-    async def _short_url_exists(self, db: AsyncSession, short_url: str) -> bool:
+    async def _long_url_exists(self, db: AsyncSession, long_url: str) -> URL:
+        result = await db.execute(select(URL).where(URL.long_url == str(long_url)))
+        return result.scalar_one_or_none()
+
+    async def _short_url_exists(self, db: AsyncSession, short_url: str) -> URL:
         result = await db.execute(select(URL).where(URL.short_url == str(short_url)))
-        return result.scalar() is not None
+        return result.scalar_one_or_none()
 
     async def short_url(self, db: AsyncSession, url_create: URLCreate) -> URL:
+        existing_url = await self._long_url_exists(db, str(url_create.long_url))
+        if existing_url:
+            return existing_url
+
         for _ in range(10): 
             short_url = ''.join(random.choices(self.CHARACTERS, k=self.SHORT_URL_LENGTH))
             if not await self._short_url_exists(db, short_url):
