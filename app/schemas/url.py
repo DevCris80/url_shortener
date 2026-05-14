f5dc0f4ac10202e5109e03bd5bc2148efd641e21
CrisPlays80 <cristian.steven05@gmail.com>
2026-05-13 19:24:03 -0500

    move: relocate schemas/url.py to app/schemas/

--- app/schemas/url.py
+++ app/schemas/url.py
@@ -1,14 +1,16 @@
-
 from pydantic import BaseModel, HttpUrl, ConfigDict
 from datetime import datetime
+
 
 class URLCreate(BaseModel):
     long_url: HttpUrl
     user_id: int
 
+
 class URLResponse(URLCreate):
     short_url: str
     clicks: int
+
 
 class URLInfo(URLResponse):
     id: int
@@ -17,6 +19,7 @@ class URLInfo(URLResponse):
     updated_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
+
 
 class URLUpdate(BaseModel):
     long_url: HttpUrl | None = None
