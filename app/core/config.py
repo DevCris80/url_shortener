4af93a3256907bb130fd2930a6591ed32d8b0878
CrisPlays80 <cristian.steven05@gmail.com>
2026-05-16 18:07:58 -0500

    Update app/core/config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DB_URL: str
+    DATABASE_URL: str
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     ALGORITHM: str
 
-    model_config = SettingsConfigDict(env_file=(".env", ".env.example"))
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
